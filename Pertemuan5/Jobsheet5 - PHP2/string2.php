c6af0186b92e29d6acb6e9bcaadfb40b90a56ce0
gelbiasa <gelbifirmansyah@gmail.com>
2024-03-18 10:05:09 +0700

    menyelesaikan jobsheet 5 file string2.php

---
+++ Pertemuan5/Jobsheet5 - PHP2/string2.php
@@ -0,0 +1,13 @@
+<?php
+
+// String 2 Escape character Langkah 3 (Soal 10)
+echo "Baris\nbaru <br>"; // Soal 10.a
+echo 'Baris\nbaru <br>'; // Soal 10.b
+echo "Halo\rDunia <br>"; // Soal 10.c
+echo 'Halo\rDunia <br>'; // Soal 10.d
+echo "<pre>Halo\tDunia !</pre>"; // Soal 10.e 
+echo '<pre>Halo\tDunia !</pre>'; // Soal 10.f 
+echo "Katakanlah \"Tidak pada narkoba!\" <br>"; // Soal 10.g
+echo 'Katakanlah \'Tidak pada narkoba!\' <br>'; // Soal 10.h
+
+?>
