c6824d72a4f30932d277400b5ff57392f4c19481
gelbiasa <gelbifirmansyah@gmail.com>
2024-03-18 09:50:17 +0700

    menyelesaikan jobsheet5 PHP2 file string1.php

---
+++ Pertemuan5/Jobsheet5 - PHP2/string1.php
@@ -0,0 +1,15 @@
+<?php
+
+// String 1 Langkah 1 (Soal 9)
+$loremIpsum = "Lorem ipsum dolor sit amet consectetur adipisicing elit.
+Voluptatem reprehenderit nobis veritatis commodi fugiat molestias
+impedit unde ipsum voluptatum, corrupti minus sit excepturi nostrum
+quisquam? Quos impedit eum nulla optio.";
+
+echo "<p>{$loremIpsum}</p>"; //Mencetak teks
+echo "Panjang karakter: " . strlen($loremIpsum) . "<br>"; //mencatat jumlah karakter pada teks
+echo "Panjang kata: " . str_word_count($loremIpsum) . "<br>"; //mencatat jumlah kata pada teks
+echo "<p>" . strtoupper($loremIpsum) . "</p>"; //Mencetak teks dengan huruf besar
+echo "<p>" . strtolower($loremIpsum) . "</p>"; //Mencetak teks dengan huruf kecil
+
+?>
