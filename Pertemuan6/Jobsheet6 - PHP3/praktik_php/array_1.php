5453c8c0c7bb5edb25af285cdec3c3833d8d669f
gelbiasa <gelbifirmansyah@gmail.com>
2024-03-20 04:57:03 +0700

    jobsheet6 Praktikum 1 file array_1.php

---
+++ Pertemuan6/Jobsheet6 - PHP3/praktik_php/array_1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!-- Praktikum Bagian 1. Indexed Array Langkah 1 -->
+<html>
+
+<head>
+</head>
+
+<body>
+    <h2>Array Terindeks</h2>
+    <?php
+    // Mendefinisikan array $Listdosen dengan daftar nama dosen
+    $Listdosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+
+    // Menampilkan nama dosen pada indeks ke-2 dari array
+    echo $Listdosen[2] . "<br>";
+    
+    // Menampilkan nama dosen pada indeks ke-0 dari array
+    echo $Listdosen[0] . "<br>";
+    
+    // Menampilkan nama dosen pada indeks ke-1 dari array
+    echo $Listdosen[1] . "<br>";
+    ?>
+</body>
+
+</html>
