296537a0bdc865ce2f8685b6d82e79e42ed9d44f
gelbiasa <gelbifirmansyah@gmail.com>
2024-03-20 05:27:03 +0700

    menyelesaikan jobsheet6  file fungsi.php

---
+++ Pertemuan6/Jobsheet6 - PHP3/praktik_php/fungsi.php
@@ -0,0 +1,15 @@
+<?php
+// Fungsi Langkah 1
+// Mendefinisikan fungsi perkenalan
+function perkenalan()
+{
+    echo "Assalamualaikum, "; // Menampilkan salam pembuka
+    echo "Perkenalkan, nama saya Gelby<br/>"; // Menampilkan nama 
+    echo "Senang berkenalan dengan Anda<br/>"; // Menampilkan pesan perkenalan
+}
+
+// Memanggil fungsi perkenalan dua kali
+perkenalan(); // Pertama kali dipanggil
+perkenalan(); // Kedua kali dipanggil
+
+?>
