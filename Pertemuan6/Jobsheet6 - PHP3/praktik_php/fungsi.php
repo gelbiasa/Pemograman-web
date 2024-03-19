e8b315871d34f856ff4a6d22adfd871fe5279efc
gelbiasa <gelbifirmansyah@gmail.com>
2024-03-20 05:34:50 +0700

    menyelesaikan jobsheet 6 file fungsi.php

--- Pertemuan6/Jobsheet6 - PHP3/praktik_php/fungsi.php
+++ Pertemuan6/Jobsheet6 - PHP3/praktik_php/fungsi.php
@@ -1,15 +1,33 @@
 <?php
-// Fungsi Langkah 1
-// Mendefinisikan fungsi perkenalan
-function perkenalan()
-{
-    echo "Assalamualaikum, "; // Menampilkan salam pembuka
-    echo "Perkenalkan, nama saya Gelby<br/>"; // Menampilkan nama 
-    echo "Senang berkenalan dengan Anda<br/>"; // Menampilkan pesan perkenalan
+// Fungsi dapat dibuat dngan kata kunci function, lalu diikuti dengan nama fungsinya.
+function perkenalan($nama, $salam = "Assalamualikum")
+{ //Parameter dengan nilai default 
+    echo $salam . ", ";
+    echo "Perkenalkan, nama saya " . $nama . "<br/>"; //Menuliskan sesuai nama
+
+    // Memanggil fungsi di dalam fungsi 
+    echo "Saya berusia ". hitungUmur(2004, 2024) . " tahun<br/>"; 
+    echo "Senang berkenalan dengan Anda<br/>";
+    // isi sesuai dengan tahun lahir kalian
 }
 
-// Memanggil fungsi perkenalan dua kali
-perkenalan(); // Pertama kali dipanggil
-perkenalan(); // Kedua kali dipanggil
+// Fungsi mengembalikan nilai 
+//membuat fungsi
+function hitungUmur($thn_lahir, $thn_sekarang)
+{
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
+}
 
+// Fungsi dengan parameter 
+//Memanggil fungsi yang sudah dibuat
+perkenalan("Hamdana", "Hallo");
+
+echo "<hr>";
+
+$saya = "Gelby";
+$ucapanSalam = "Selamat pagi";
+
+//memanggil lagi tanpa mengisi parameter salam
+perkenalan($saya);
 ?>
