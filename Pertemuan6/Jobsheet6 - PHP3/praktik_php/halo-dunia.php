5245a697d1b09238937d913aae0774ccdac96df4
gelbiasa <gelbifirmansyah@gmail.com>
2024-03-20 07:50:04 +0700

    menyelesaikan variabel get file halo-dunia.php

---
+++ Pertemuan6/Jobsheet6 - PHP3/praktik_php/halo-dunia.php
@@ -0,0 +1,8 @@
+<!-- Variabel $_GET -->
+<?php
+
+$nama = @$_GET['nama']; //tanda @ agar tidak ada peringatan error ketika key-nya kosong
+$usia = @$_GET['usia']; //tanda @ agar tidak ada peringatan error ketika key-nya kosong
+
+echo "Halo {$nama}! Apakah benar anda berusia {$usia} tahun?"; // Menampilkan kalimat dengan nilai variabel $nama dan $usia
+?>
