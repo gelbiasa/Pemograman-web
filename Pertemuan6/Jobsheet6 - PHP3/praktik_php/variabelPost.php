fd51e393f5fb38cbc8e986a022fd47818954142f
gelbiasa <gelbifirmansyah@gmail.com>
2024-03-20 08:09:11 +0700

    menyelesaikan variabelPost pada jobsheet 6

--- Pertemuan6/Jobsheet6 - PHP3/praktik_php/variabelPost.php
+++ Pertemuan6/Jobsheet6 - PHP3/praktik_php/variabelPost.php
@@ -1,20 +1,25 @@
+<!-- Variabel $_POST mirip dengan variabel $_GET. Hanya saja data yang di-passing 
+tidaklah melalui query string pada URL, akan tetapi pada body request. Dan request 
+method yang dilakukan haruslah dengan metode POST. -->
 <html>
 
 <body>
-
-    <form method="post" action=" <?php echo $_SERVER['PHP_SELF']; ?>">
-        Name: <input type="text" name="fname">
-        <input type="submit">
+    <!-- Formulir untuk memasukkan nama -->
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        Name: <input type="text" name="fname"> <!-- Input field untuk nama -->
+        <input type="submit"> <!-- Tombol submit untuk mengirimkan formulir -->
     </form>
 
     <?php
+    // Memeriksa apakah formulir telah di-submit dengan metode POST
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // collect value of input field
+        // Mengumpulkan nilai input field
         $name = $_POST['fname'];
-        if (empty ($name)) {
-            echo "Name is empty";
+        // Memeriksa apakah input kosong atau tidak
+        if (empty($name)) {
+            echo "Name is empty"; // Menampilkan pesan jika input kosong
         } else {
-            echo $name;
+            echo $name; // Menampilkan nama yang dimasukkan jika tidak kosong
         }
     }
     ?>
