f2392e4a467a44ba8bac252fc70c18d59c3de00f
gelbiasa <gelbifirmansyah@gmail.com>
2024-03-22 08:41:13 +0700

    jobsheet 7 Praktikum 3 file form.php

---
+++ Pertemuan7/Jobsheet7/form.php
@@ -0,0 +1,17 @@
+<!-- Praktikum Bagian 3 : Form Input PHP -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form Input PHP</title> <!-- Judul halaman web -->
+</head>
+<body>
+    <h2>Form Input PHP</h2> <!-- Heading/Formulir judul -->
+    <form method="post" action="proses_form.php"> <!-- Membuat formulir dengan metode POST yang akan di-submit ke file 'proses_form.php' -->
+        <label for="nama">Nama:</label> <!-- Label untuk input nama -->
+        <input type="text" name="nama" id="nama" required><br><br> <!-- Input untuk nama dengan atribut required -->
+        <label for="email">Email:</label> <!-- Label untuk input email -->
+        <input type="email" name="email" id="email" required><br><br> <!-- Input untuk email dengan atribut required dan tipe 'email' -->
+        <input type="submit" name="submit" value="Submit"> <!-- Tombol submit -->
+    </form>
+</body>
+</html>
