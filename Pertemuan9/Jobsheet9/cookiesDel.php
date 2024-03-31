aa0652c1c3118ee40c09fe35efc6b9b2c1deea2e
gelbiasa <gelbifirmansyah@gmail.com>
2024-03-31 13:12:02 +0700

    Menyelesaikan Praktikum 6 Jobhseet 9 Langkah 1

---
+++ Pertemuan9/Jobsheet9/cookiesDel.php
@@ -0,0 +1,5 @@
+<!-- Praktikum 6 Jobhseet 9 Langkah 1 -->
+<?php
+    // Menghapus cookie dengan nama "user" dengan mengatur waktu kedaluwarsa ke masa lampau
+    setcookie("user", "Polinema", time() -3600);
+?>
