1b8d75740d7b1714df3163f5cd9b4ba5b5aeb0d4
gelbiasa <gelbifirmansyah@gmail.com>
2024-03-31 13:06:06 +0700

    Menyelesaikan Praktikum 5 Jobhseet 9 Langkah 2

---
+++ Pertemuan9/Jobsheet9/cookiesCreate.php
@@ -0,0 +1,5 @@
+<!-- Praktikum 5 Jobhseet 9 Langkah 2 -->
+<?php
+    // Membuat cookie dengan nama "user" dan nilai "Polinema" yang akan berlaku selama 1 jam (3600 detik)
+    setcookie("user", "Polinema", time()+3600);
+?>
