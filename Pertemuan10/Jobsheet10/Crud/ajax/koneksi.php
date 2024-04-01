de4ef11fe01e276a262a6c8a1f9f2c7025caf005
gelbiasa <gelbifirmansyah@gmail.com>
2024-04-01 23:14:35 +0700

    Menyelesaikan Prak 6 JS 10 Crud koneksi.php

---
+++ Pertemuan10/Jobsheet10/Crud/ajax/koneksi.php
@@ -0,0 +1,11 @@
+<!-- Praktikum 6. Tampilan CRUD dengan Ajax Langkah 3 -->
+<?php
+// Mendefinisikan konstanta untuk koneksi ke database
+define('HOST','localhost'); // Nama host database
+define('USER','root'); // Username database
+define('PASS',''); // Password database
+define('DB1','prakwebdb'); // Nama database
+
+// Koneksi ke database menggunakan objek mysqli
+$db1 = new mysqli(HOST, USER, PASS, DB1);
+?>
