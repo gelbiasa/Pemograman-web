691547f9cfd82899b961e19527d20da131ea8f15
gelbiasa <gelbifirmansyah@gmail.com>
2024-04-07 14:37:13 +0700

    melakukan penambahan kode pada file hapus.php

--- Pertemuan11/webprograming2024/fungsi/hapus.php
+++ Pertemuan11/webprograming2024/fungsi/hapus.php
@@ -1,4 +1,3 @@
-<!-- Praktikum 4. Folder Module – Bagian Jabatan Langkah 6 hapus.php -->
 <?php
 // Memulai sesi
 session_start();
@@ -31,6 +30,35 @@ if (!empty($_SESSION['username'])) {
         
         // Redirect kembali ke halaman jabatan
         header("Location: ../index.php?page=jabatan");
+    } elseif (!empty($_GET['anggota'])) {
+        // Jika parameter id kosong tetapi parameter anggota tidak kosong
+        
+        // Mengambil id user dan mencegah SQL injection
+        $id = antiinjection($koneksi, $_GET['id']);
+        
+        // Query untuk menghapus data user dari database
+        $query = "DELETE FROM user WHERE id = '$id'";
+        
+        // Menjalankan query
+        if (mysqli_query($koneksi, $query)) {
+            // Jika berhasil, lanjutkan dengan menghapus data anggota terkait
+            $query2 = "DELETE FROM anggota WHERE user_id = '$id'";
+            
+            // Menjalankan query
+            if (mysqli_query($koneksi, $query2)) {
+                // Jika berhasil, tampilkan pesan sukses
+                pesan('success', "Anggota Telah Terhapus.");
+            } else {
+                // Jika gagal, tampilkan pesan warning
+                pesan('warning', "Data Login Terhapus Tetapi Data Anggota Tidak Terhapus Karena: " . mysqli_error($koneksi));
+            }
+        } else {
+            // Jika gagal, tampilkan pesan error beserta pesan error dari MySQL
+            pesan('danger', "Anggota Tidak Terhapus Karena: " . mysqli_error($koneksi));
+        }
+        
+        // Redirect kembali ke halaman anggota
+        header("Location: ../index.php?page=anggota");
     }
 }
 ?>
