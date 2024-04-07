74ac98709a6b7f0d32eb6d20e002d93015a96841
gelbiasa <gelbifirmansyah@gmail.com>
2024-04-07 13:15:37 +0700

    Menyelesaikan Prak 2 JS 11 anti_injection.php

---
+++ Pertemuan11/webprograming2024/fungsi/anti_injection.php
@@ -0,0 +1,10 @@
+<!-- Praktikum 2. Folder Fungsi Langkah 2 anti_injection.php -->
+<?php
+    // Fungsi antiinjection() untuk mencegah serangan SQL injection
+    function antiinjection($koneksi, $data){
+        // Membersihkan dan menghindari karakter khusus dari data yang diterima sebelum diproses
+        $filter_sql =  mysqli_real_escape_string($koneksi, stripslashes(strip_tags(htmlspecialchars($data, ENT_QUOTES))));
+        // Mengembalikan data yang telah dibersihkan
+        return $filter_sql;
+    }
+?>
